8ffcc2ec4a749f1138c733ae659bdd6424050318
djns <130430285+DDJCS50@users.noreply.github.com>
2023-07-05 11:10:44 -0700

    Add elements and modify README
    Added a pizza img to the project and altered the page.js file in order
    to append it to the page. Also added logic to the page.js file in order
    to create and append a title in the header for the restaurant name
    and a testimonial in the main section. Added styling to the img and the
    page overall to begin making it look nicer

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # restaurant-page
 
 The goal of this project is to create a simple webpage using html, css, javascript, and webpack.
+<br>
+[pizza](./src/pizza.jpg) Photo by <a href="https://unsplash.com/@iavnt?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ivan Torres</a> on <a href="https://unsplash.com/images/food/pizza?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
--- src/page.js
+++ src/page.js
@@ -1,8 +1,26 @@
+import pizza from './pizza.jpg';
+
 export default function makeStarterPage() {
   const content = document.querySelector('#content');
   const header = document.createElement('header');
   const main = document.createElement('main');
   const footer = document.createElement('footer');
+  
+  const myPizza = new Image();
+  myPizza.src = pizza;
+  myPizza.alt = 'picture of pizza';
+  
+  const headline = document.createElement('h1');
+  headline.innerText = 'Pepino\'s Pizza';
+
+  const testimonial = document.createElement('div');
+  testimonial.setAttribute('class', 'card');
+  testimonial.innerText = 'Pepino\'s Pizza is the best!';
+  
+
+  header.appendChild(headline);
+  main.appendChild(myPizza);
+  main.appendChild(testimonial);
 
   content.appendChild(header);
   content.appendChild(main);
